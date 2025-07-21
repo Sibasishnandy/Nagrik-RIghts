861480534c49f65d26a9c845a67ade0c8c0d3232
Sibasishnandy <142724809+Sibasishnandy@users.noreply.github.com>
2025-07-21 08:53:43 +0530

    Update Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -35,7 +35,7 @@ export default function Home() {
     setIsTyping(true);
 
     try {
-      const res = await fetch('http://nagrik-rights-basic1.onrender.com/ask_gemini', {
+      const res = await fetch('https://nagrik-rights-basic1.onrender.com/ask_gemini', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
